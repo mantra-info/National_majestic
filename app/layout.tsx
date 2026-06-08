f9a839bb25d4dae72de5a40dc9acfba89fa32757
mantra-info <albie@mantraitsolutions.in>
2026-06-08 10:14:29 +0530

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Microsoft Clarity */}
+        <Script id="clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "x1oxpwuwe6");`}
+        </Script>
         {/* Google Tag Manager */}
         <Script id="gtm-head" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
